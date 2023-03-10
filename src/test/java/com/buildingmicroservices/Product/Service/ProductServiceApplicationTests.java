ffbe574a2e3a3f573c508f98bc6bccb163c74b23
Don <kibetdonald97@gmail.com>
2023-03-10 12:52:02 +0300

    Added test containers

--- src/test/java/com/buildingmicroservices/Product/Service/ProductServiceApplicationTests.java
+++ src/test/java/com/buildingmicroservices/Product/Service/ProductServiceApplicationTests.java
@@ -2,10 +2,15 @@ package com.buildingmicroservices.Product.Service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
 
+//	@Container
+//	static MongoDBContainer mongoDBContainer new MongoDBContainer("mongo: 4.4.2");
 	@Test
 	void contextLoads() {
 	}
